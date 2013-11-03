6fb7d831fe451ab3e60a8018f8ce8d48eab3120b
MaxCobMara <maxcobmara@gmail.com>
2013-11-03 17:16:57 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Txport::Application.config.secret_key_base = '07e7641b19ee682e3f8838002839ef52dfc90518b0ecb23e41c7e1ce374ad74ec62460995df8ae4377061cb509aef219317e3b895ca5a6eb574d39d49c0a988c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Txport::Application.config.session_store :cookie_store, key: '_txport_session'
