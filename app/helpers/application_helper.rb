a927c77947c518df2265b167b49848b070321296
MaxCobMara <maxcobmara@gmail.com>
2013-11-04 12:20:38 +0800

    finish show pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+  
+  #Turns decimal into Ringgit
+  def ringgols(money)
+    number_to_currency(money, :unit => "RM ", :separator => ".", :delimiter => ",", :precision => 2)
+  end
 end
