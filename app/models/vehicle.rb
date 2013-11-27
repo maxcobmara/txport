9e48dfbf91a647de2a239605b062f914abc96a3f
MaxCobMara <maxcobmara@gmail.com>
2013-11-28 05:55:20 +0800

    some fixes

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -4,9 +4,9 @@ class Vehicle < ActiveRecord::Base
   validates_uniqueness_of :chassis_no,  :allow_nil => true, :allow_blank => true
   validates_uniqueness_of :engine_no,   :allow_nil => true, :allow_blank => true
   
-  has_many :vehicle_road_taxes
-  has_many :vehicle_fines
-  has_many :maintenances
+  has_many :vehicle_road_taxes, :dependent => :destroy
+  has_many :vehicle_fines,      :dependent => :destroy   
+  has_many :maintenances,       :dependent => :destroy
   belongs_to :card,     :class_name => "VehicleCard",     :foreign_key => "vehicle_id"
   belongs_to :status,   :class_name => "VehicleStatus",   :foreign_key => "status_id"
   
