afdb125d6152c7ed5b06530ac0db4627079b9ec6
MaxCobMara <maxcobmara@gmail.com>
2013-11-30 16:25:45 +0800

    fixes for road tax, last unique search and status

--- app/models/vehicle_road_tax.rb
+++ app/models/vehicle_road_tax.rb
@@ -1,3 +1,13 @@
 class VehicleRoadTax < ActiveRecord::Base
 	belongs_to :vehicle
+  
+  def status
+    if end_on < Date.today
+      "error"
+    elsif end_on < Date.today + 3.months
+      "warning"
+    else
+      "success"
+    end
+  end
 end
