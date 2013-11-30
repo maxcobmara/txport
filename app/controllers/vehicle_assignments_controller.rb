9808399b84067e70db7d06e0cf7c574a66781a48
MaxCobMara <maxcobmara@gmail.com>
2013-11-30 23:23:06 +0800

    fixes for vehicle assignment

--- app/controllers/vehicle_assignments_controller.rb
+++ app/controllers/vehicle_assignments_controller.rb
@@ -69,6 +69,6 @@ class VehicleAssignmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def vehicle_assignment_params
-      params.require(:vehicle_assignment).permit(:vehicle_id, :staff_id, :assigned_on, :assignment_end)
+      params.require(:vehicle_assignment).permit(:document_code, :document_date, :authorised_by)
     end
 end
