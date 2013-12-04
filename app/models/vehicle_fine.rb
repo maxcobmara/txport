f7a7be6dae7e4a2675a2278429c92abfe773491e
MaxCobMara <maxcobmara@gmail.com>
2013-12-05 01:09:56 +0800

    add fine payment status and some fixes

--- app/models/vehicle_fine.rb
+++ app/models/vehicle_fine.rb
@@ -4,7 +4,7 @@ class VehicleFine < ActiveRecord::Base
  belongs_to :vehicle
  belongs_to :vehicle_fine_type, :foreign_key => "type_id"
  
- def status
+ def table_status
    if paid_on != nil
      "info" #paid
    elsif pay_before < Date.today
@@ -13,6 +13,18 @@ class VehicleFine < ActiveRecord::Base
      "warning" #due
    else
      "success" #ok
+   end
+ end
+ 
+ def payment_status
+   if paid_amount < compound && receipt_no.length > 1
+     "Diskaun"
+   elsif paid_amount == compound && receipt_no.length > 1
+     "Bayaran Penuh"
+   elsif paid_amount < 1 && receipt_no.length > 1
+     "Dikecualikan"
+   else
+     "Tiada Bayaran"
    end
  end
 
