457a5aae3a533861d7759ce606b9bec85863ecfa
MaxCobMara <maxcobmara@gmail.com>
2013-12-10 02:54:51 +0800

    vehicle assignment complete

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,4 +1,6 @@
 class Staff < ActiveRecord::Base
-  has_many :contracts,   foreign_key: "issued_by"
-	belongs_to :rank, :foreign_key => "rank_id"  
+  has_many :contracts,                            foreign_key: "issued_by"
+  has_many :authorisations, class_name: "Staff",  foreign_key: "authorised_by"
+	
+  belongs_to :rank, :foreign_key => "rank_id"  
 end
--- app/models/vehicle_assignment.rb
+++ app/models/vehicle_assignment.rb
@@ -1,4 +1,6 @@
 class VehicleAssignment < ActiveRecord::Base
+  belongs_to :authoriser, class_name: "Staff", foreign_key: "authorised_by"
+  
   has_many :vehicle_assignment_details, :foreign_key => 'vehicle_assignment_id' 
-  accepts_nested_attributes_for :vehicle_assignment_details, allow_destroy: true
+  accepts_nested_attributes_for :vehicle_assignment_details, allow_destroy: true, reject_if: proc { |vehicle_assignment_details| vehicle_assignment_details[:vehicle_id].blank? }
 end
