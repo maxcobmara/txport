02f80bce1f3d0c6c7242411e74dc4189167d211c
MaxCobMara <maxcobmara@gmail.com>
2014-01-17 20:50:00 +0800

    Fixes for Unit

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,5 @@
 class Unit < ActiveRecord::Base
   has_many :fuel_tanks, :foreign_key => "unit_id"
+  has_many :subunits, class_name: "Unit", foreign_key: 'parent_id'
+  belongs_to :parent, class_name: "Unit", foreign_key: 'parent_id'
 end
