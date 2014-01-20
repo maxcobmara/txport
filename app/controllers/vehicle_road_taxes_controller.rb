57d5cd5137dfe80ef94cb9ff17a6e87085e40ba0
irshad89 <isyaduddin@gmail.com>
2014-01-20 15:39:09 +0800

    report road tax

--- app/controllers/vehicle_road_taxes_controller.rb
+++ app/controllers/vehicle_road_taxes_controller.rb
@@ -62,7 +62,11 @@ class VehicleRoadTaxesController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
+  def monthly_roadtax
+    @vehicle_road_taxes = VehicleRoadTax.all
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_vehicle_road_tax
