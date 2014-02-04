937732caf44c8709cd5379444e2e205d003ff0f3
Shima <shima@MacBook-2.local>
2014-02-04 14:51:17 +0800

    table migration for staff-default skala by rank

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,3 +1,18 @@
 class Rank < ActiveRecord::Base
-	has_many :staffs
+	has_many :staffs, dependent: :nullify
+  
+  RATE = [
+             #  Displayed       stored in db
+             ["Junior Rate", 1],
+             ["Senior Rate", 2],
+             ["Kadet Kanan & Ke Bawah", 3],
+             ["Leftenan Muda & Ke Atas", 4]
+           ]
+
+  CATEGORY = [
+            #  Displayed       stored in db
+              ["Lain-Lain Pangkat", 1],
+              ["Pegawai", 2]
+           ]         
+  
 end
