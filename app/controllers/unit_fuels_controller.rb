a42c950e25384fdcb9e41db09125faeffc8ccb83
irshad89 <isyaduddin@gmail.com>
2014-03-20 12:29:02 +0800

    fix annual report

--- app/controllers/unit_fuels_controller.rb
+++ app/controllers/unit_fuels_controller.rb
@@ -86,7 +86,8 @@ class UnitFuelsController < ApplicationController
      @year_annual_usage_report = UnitFuel.where( "issue_date >= ? AND issue_date <= ? ", @sdate, @edate ) 
      @year_other_fuel = AddFuel.where( "created_at >= ? AND created_at <= ? ", @sdate, @edate )
      @year_external_supply = ExternalSupplied.where( "created_at >= ? AND created_at <= ? ", @sdate, @edate )
-     @unit_fuels = UnitFuel.where( "issue_date >= ? AND issue_date <= ? ", @sdate, @edate ) 
+     @unit_fuels = UnitFuel.where( "issue_date >= ? AND issue_date <= ? ", @sdate, @edate )
+     @add_fuels = AddFuel.where( "issue_date >= ? AND issue_date <= ? ", @sdate, @edate )
   end
   
   def daily_usage  
