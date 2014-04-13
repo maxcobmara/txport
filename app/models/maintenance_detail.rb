cf611df79d9e27e8d75e30db74cb46331f47aa83
nurhashimah <nurhashimah77@gmail.com>
2014-04-13 20:09:28 +0800

    Fixes for maintenance - work order no & new Excel Format for vehicle & maintenance

--- app/models/maintenance_detail.rb
+++ app/models/maintenance_detail.rb
@@ -7,6 +7,21 @@ class MaintenanceDetail < ActiveRecord::Base
     line_item_price * quantity
   end
 
+  #did not works in helper
+  def self.get_quantity(mparts)
+    fulldesc_arr = mparts.split(" ")                        # ["partsA", "01nos"] if ..... partsA 01nos   @ "partsA 01 nos"
+    last_item = fulldesc_arr[fulldesc_arr.count-1]          # "01nos"                                     @ "nos"
+    second_last_item = fulldesc_arr[fulldesc_arr.count-2]
+    qty_type_arr = last_item.scan(/\d+|\D+/)                # ["01", "nos"]                               @ "nos"
+    qty_type_arr_count = qty_type_arr.count                 # ["01", "nos"].count ==> 2                   @ 1
+    if qty_type_arr_count>1    
+      qty = qty_type_arr[0].to_i
+    else
+      qty = second_last_item.to_i
+    end
+    qty
+  end 
+
 end
 
 # == Schema Information
