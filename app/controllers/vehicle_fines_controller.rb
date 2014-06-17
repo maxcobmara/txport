09c3afa6988ca942f5ce31e47235df5d8a8f5180
MaxCobMara <maxcobmara@gmail.com>
2014-06-17 23:18:16 +0800

    make controller use xls

--- app/controllers/vehicle_fines_controller.rb
+++ app/controllers/vehicle_fines_controller.rb
@@ -10,7 +10,7 @@ class VehicleFinesController < ApplicationController
     respond_to do |format|
       format.html
       format.csv { send_data @vehicle_fines.to_csv }
-      format.xls { send_data @vehicle_fines.to_csv(col_sep: "\t") }
+      format.xls
   end
 end
 
