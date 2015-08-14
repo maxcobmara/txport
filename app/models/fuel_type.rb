b0ef621566bb0fb2e81157807f8afdfd6a38e9b2
Nurhashimah <nurhashimah77@gmail.com>
2015-08-14 15:12:25 +0800

    Vehicle revised - limit type to 4 main fuel type

--- app/models/fuel_type.rb
+++ app/models/fuel_type.rb
@@ -7,6 +7,8 @@ class FuelType < ActiveRecord::Base
   has_many :external_supplieds, dependent: :nullify
   has_many :vehicles,  dependent: :nullify
   
+  scope :main_use, -> { where(name: ['PETROL', 'DIESEL', 'AVTUR', 'AVCAT'])}
+  
   def self.exclude_petrol_diesel
     where('name LIKE (?) OR name LIKE (?)','PETROL','DIESEL').pluck(:id)
   end
