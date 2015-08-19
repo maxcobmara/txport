a6431824d48b49fddf7142420ae2d49af190bc08
Nurhashimah <nurhashimah77@gmail.com>
2015-08-20 02:00:29 +0800

    Unit Fuel reactivate

--- app/models/fuel_type.rb
+++ app/models/fuel_type.rb
@@ -7,7 +7,7 @@ class FuelType < ActiveRecord::Base
   has_many :external_supplieds, dependent: :nullify
   has_many :vehicles,  dependent: :nullify
   
-  scope :main_use, -> { where(name: ['PETROL', 'DIESEL', 'AVTUR', 'AVCAT'])}
+  scope :main_use, -> { where(id: [FuelTank.where('fuel_type_id is not null').pluck(:fuel_type_id)])} #for use in Vehicle - fuel_type
   
   def self.exclude_petrol_diesel
     where('name LIKE (?) OR name LIKE (?)','PETROL','DIESEL').pluck(:id)
