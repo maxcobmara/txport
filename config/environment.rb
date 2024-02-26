54da857bbc8f08bb24b4fe27ded8875937d97dca
Yaacob Noor <maxcobmara@gmail.com>
2024-02-26 18:09:18 +0800

    change env.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
-require File.expand_path('../application', __FILE__)
+#require File.expand_path('../application', __FILE__). <-- deprecated rails 5.2
+require_relative 'application'
+
+
 
 # Initialize the Rails application.
 Txport::Application.initialize!
